7cd08643b9a29710329ce7276b2e2053efa8e474
Adriano <russoadriano92@gmail.com>
2023-02-18 19:50:08 +0100

    update form

---
+++ assets/js/script.js
@@ -0,0 +1,33 @@
+function SendMail() {
+    var params = {
+      from_name: document.getElementById("nome").value,
+      email_id: document.getElementById("email").value,
+      message: document.getElementById("testo").value
+    };
+  
+    const serviceID = "service_kyotvvj";
+    const templateID = "template_uiiep7i";
+  
+      emailjs.send(serviceID, templateID, params)
+      .then(res=>{
+          document.getElementById("nome").value = "";
+          document.getElementById("email").value = "";
+          document.getElementById("testo").value = "";
+          alert("Messaggio inviato correttamente")
+  
+      })
+  
+  }
+
+
+//   function SendMail() {
+//     var params = {
+//       from_name: document.getElementById("nome").value,
+//       email_id: document.getElementById("email").value,
+//       message: document.getElementById("testo").value
+//     }
+//     emailjs.send("service_kyotvvj", "template_uiiep7i", params).then(function (res){
+//         alert("Messaggio inviato")
+//     })
+  
+//   }
