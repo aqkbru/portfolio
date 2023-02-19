2e20ae2ec4b17f275a0df82d7d5000da95bbb952
Adriano <russoadriano92@gmail.com>
2023-02-19 11:55:08 +0100

    update menu

--- assets/js/script.js
+++ assets/js/script.js
@@ -17,7 +17,17 @@ function SendMail() {
   
       })
   
+
+}
+
+function togglemenu() {
+  const menu = document.getElementById("menu");
+  if (menu.style.display === "block") {
+    menu.style.display = "none";
+  } else {
+    menu.style.display = "block";
   }
+}
 
 //   function SendMail() {
 //     var params = {
